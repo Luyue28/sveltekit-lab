0f348e03b85b0d4727b7bbeb4efec42178d2eb79
Luyue28 <zhan0067@hz.nl>
2024-10-02 21:57:10 +0200

    filter pets based on type

--- frontend-mock-exam/src/routes/+page.svelte
+++ frontend-mock-exam/src/routes/+page.svelte
@@ -1,16 +1,16 @@
 <script>
-	import Cats from '$lib/components/Cats.svelte';
+	import Pets from "$lib/components/Pets.svelte";
 </script>
 
 <div class="container w-full mx-auto">
     <section class="mt-3 lg:mt-6">
 		<h2 class="text-4xl lg:text-6xl">
-			C<span class="inline-block bg-blue-100 rounded-lg">ats&nbsp;</span>
+			A<span class="inline-block bg-blue-100 rounded-lg">ll pets&nbsp;</span>
 		</h2>
 	</section>
     <section class="flex flex-col lg:flex-row mt-3 lg:mt-6">
         <div class="basis-4/6 bg-gray-100 rounded-lg p-10 lg:mr-8">
-            <Cats />
+            <Pets />
         </div>
     </section>
 </div>
